4654dd29a8003b18df12f3752479c87c6c979d87
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-01-07 23:49:56 -0800

    Fixed up code signing to work with iPad mini and added a paid vs free file

--- Theremin/VWWThereminConfiguration.h
+++ Theremin/VWWThereminConfiguration.h
@@ -14,24 +14,41 @@
 
 // Temporary in order to not render for release 1.0
 #define VWW_SKIP_RENDERING_CALL     1
+#if defined(VWW_ENABLE_PAID_VERSION)
+    #define VWW_FREQUENCY_MAX           2500.0
+    #define VWW_FREQUENCY_MIN           20.0
+    #define VWW_INPUT_FREQUENCY_MAX     2000.0
+    #define VWW_INPUT_FREQUENCY_MIN     30.0
+    #define VWW_WAVETYPE                kWaveSin
+    #define VWW_SENSITIVITY             1.0
+    #define VWW_EFFECT                  kEffectNone
+    #define VWW_INPUT_TYPE              kInputTouch
+    #define VWW_AMPLITUDE               1.0
+    #define VWW_NOTE_KEY                kNoteKeyChromatic
+    #define VWW_MUTED                   YES
+    #define VWW_SHOW_INFO_SCREENS       1
+    #define VWW_MAX_CHANNELS            1
+    #define VWW_DISMISS_INFO_DURATION   0.5
+#else
+    #define VWW_FREQUENCY_MAX           2500.0
+    #define VWW_FREQUENCY_MIN           20.0
+    #define VWW_INPUT_FREQUENCY_MAX     2000.0
+    #define VWW_INPUT_FREQUENCY_MIN     30.0
+    #define VWW_WAVETYPE                kWaveSin
+    #define VWW_SENSITIVITY             1.0
+    #define VWW_EFFECT                  kEffectNone
+    #define VWW_INPUT_TYPE              kInputTouch
+    #define VWW_AMPLITUDE               1.0
+    #define VWW_NOTE_KEY                kNoteKeyChromatic
+    #define VWW_MUTED                   YES
+    #define VWW_SHOW_INFO_SCREENS       1
+    #define VWW_MAX_CHANNELS            1
+    #define VWW_DISMISS_INFO_DURATION   0.5
+#endif // VWW_ENABLE_PAID_VERSION
 
 
-// Max/min frequency that can be reached by any channel
-#define VWW_FREQUENCY_MAX           2500.0
-#define VWW_FREQUENCY_MIN           20.0
 
-// Default max/min for each channel
-#define VWW_INPUT_FREQUENCY_MAX     2000.0
-#define VWW_INPUT_FREQUENCY_MIN     30.0
-#define VWW_WAVETYPE                kWaveSin
-#define VWW_SENSITIVITY             1.0
-#define VWW_EFFECT                  kEffectNone
-#define VWW_INPUT_TYPE              kInputTouch
-#define VWW_AMPLITUDE               1.0
-#define VWW_NOTE_KEY                kNoteKeyChromatic
-#define VWW_MUTED                   YES
-#define VWW_SHOW_INFO_SCREENS       1
-#define VWW_MAX_CHANNELS            1
 
-#define VWW_DISMISS_INFO_DURATION   0.5
-#endif
+
+
+#endif // Theremin_VWWThereminConfiguration_h
